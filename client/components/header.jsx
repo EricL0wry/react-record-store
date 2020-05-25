5d6ebd081c7cca519ad9b915fcbe331de3b8a6bd
Eric Lowry <ericmichaellowry@gmail.com>
2020-05-25 16:58:35 -0700

    Updated nav to include logo, fixed positioning, and proper responsive spacing.

--- client/components/header.jsx
+++ client/components/header.jsx
@@ -4,14 +4,13 @@ function Header(props) {
   const { cartItemCount } = props;
 
   return (
-    <nav className="navbar navbar-dark bg-dark">
-      <div className="container">
-        <a href="#" className="navbar-brand ml-1">
-          <i className="fas fa-broom mr-2"></i>
-        Wicked Sales
-        </a>
+    <nav className="navbar navbar-dark bg-dark fixed-top">
+      <div className="container px-0 px-sm-3">
+        <span className="navbar-brand ml-1" onClick={() => props.setView('catalog', {})}>
+          <img src="/images/IntoTheGroove.svg" alt="Brand Logo" className="logo"/>
+        </span>
         <div
-          className="cart navbar-brand d-flex flex-row"
+          className="cart navbar-brand d-flex flex-row mr-1"
           onClick={() => props.setView('cart', {})}
         >
           <h6 className="mb-0">{`${cartItemCount} Items`}</h6>
